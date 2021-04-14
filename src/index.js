9de511294c7c4caf1e342adf7795c99002ee2386
limh1 <limh1@bestone.com>
2021-04-14 18:42:57 +0800

    feature: 增加日历组件

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/**
+ * @author jason
+ * Date: 2021/04/13
+ */
+import Calendar from './calendar/index.vue';
+
+const components = [
+  Calendar,
+]
+
+const install = function(Vue) {
+  if (install.installed) return
+  components.map(component => Vue.component(component.name, component))
+ 
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default {
+  install,
+  Calendar
+}
